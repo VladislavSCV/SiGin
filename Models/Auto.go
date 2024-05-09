0ed5c52b7c685725584e2efee1ce89f7f349d339
VLADislav314 <vladnety134@gmail.com>
2024-05-09 19:38:18 +0300

    edit auto and user structure

--- Models/Auto.go
+++ Models/Auto.go
@@ -1,9 +1,49 @@
 package models
 
+import "fmt"
+
 type Auto struct {
 	Brand string `json:"brand"`
 	Model string `json:"model"`
 	Color string `json:"color"`
-	Year int `json:"year"`
-	Price int `json:"price"`
+	Year  int    `json:"year"`
+	Price int    `json:"price"`
+}
+
+var autos = make(map[uint]Auto)
+
+func GetAutos() (autosList []Auto) {
+	for _, a := range autos {
+		autosList = append(autosList, a)
+	}
+	return
+}
+
+func GetAuto(id uint) (auto Auto, err error) {
+	if a, ok := autos[id]; ok {
+		return a, nil
+	}
+	return Auto{}, fmt.Errorf("Auto with id %d not found", id)
+}
+
+func CreateAuto(auto Auto) (status int) {
+	autos[uint(len(autos)+1)] = auto
+	return 1
+}
+
+func UpdateAuto(id uint, autoToUpdate Auto) (auto Auto, err error) {
+	if _, ok := autos[id]; ok {
+		auto = autoToUpdate
+		autos[id] = auto
+		return auto, nil
+	}
+	return Auto{}, fmt.Errorf("Auto with id %d not found", id)
+}
+
+func DeleteAuto(id uint) (status int) {
+	if _, ok := autos[id]; ok {
+		delete(autos, id)
+		return 1
+	}
+	return 0
 }
--- Models/User.go
+++ Models/User.go
@@ -1,10 +1,47 @@
 package models
 
+import "fmt"
+
 type User struct {
-	Id uint `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
+	Id       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-var UsersDB = make(map[uint]User);
+var UsersDB = make(map[uint]User)
+
+func GetUsers() (users []User) {
+	users = make([]User, 0, len(UsersDB))
+	for _, u := range UsersDB {
+		users = append(users, u)
+	}
+	return
+}
+
+func GetUserById(id uint) (user User) {
+	user = UsersDB[id]
+	return
+}
+
+func UpdateUserById(id uint, userToUpdate User) (user User, err error) {
+	if user, ok := UsersDB[id]; ok {
+		user = userToUpdate
+		UsersDB[id] = user
+		return user, nil
+	}
+	return User{}, fmt.Errorf("User with id %d not found", id)
+}
+
+func AddUser(user User) (status int) {
+	UsersDB[uint(len(UsersDB)+1)] = user
+	return 1
+}
+
+func DeleteUser(id uint) (status int) {
+	if _, ok := UsersDB[id]; ok {
+		delete(UsersDB, id)
+		return 1
+	}
+	return 0
+}
